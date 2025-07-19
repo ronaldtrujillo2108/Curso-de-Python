7e2a53363ce08e3334e12954bea28e40956b0e07
ronaldtrujillo2108 <cocollante2108@gmail.com>
2025-07-19 16:53:24 -0400

    hola

---
+++ EJercicios Resueltos.py
@@ -0,0 +1,62 @@
+#Ejercicio 1
+Saludo = "Hola mi nombre es"
+Nombre = "Ronald mTrujillo"
+print(Saludo,Nombre)
+
+#Ejercicio 2
+A = 10
+B = 20
+SUMA = A + B 
+print(SUMA)
+
+#Ejercicio 3
+Usuario =input("ingresa tu nombre")
+print(f"Hola mi nombre es {Usuario}")
+
+#Ejercicio 4
+Comida_Favorita = "Hamburguesas"
+print(Comida_Favorita)
+
+#Ejercicio 5
+C = 5
+D = 15
+RESTA = C - D 
+print(RESTA)
+
+#Ejercicio 6
+Edad = input("Cuantos años tienes")
+print(f"Tu edad es {Edad}")
+
+#Ejercicio 7
+Mascota=input("Ingresa el nombtre de tu Mascota")
+print(f"tu mascota se llama {Mascota}")
+
+#Ejercicio 8
+numero1 = 3
+numero2 = 4
+MULTIPLICACION = numero1 * numero2 
+print(MULTIPLICACION)
+
+#Ejercicio 9
+Color =input("ingresa tu Color Favorito")
+print(f"¡Qué buen gusto! El {Color} es genial.")
+
+
+#Ejercicio 10
+def mostrar_letra():
+    letra = """
+    In this farewell
+    There's no blood, there's no alibi
+    'Cause I've drawn regret
+    From the truth of a thousand lies...
+    """
+    print("🎵 Letra de 'What I've Done' - Linkin Park 🎵\n")
+    print(letra)
+
+# Ejecutar la función
+mostrar_letra()
+
+
+
+
+
